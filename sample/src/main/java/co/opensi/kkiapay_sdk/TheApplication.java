6478f595583688a00d714dfbef2688cb2ad5e581
Armel <armel.fagbedji@mehriso.com>
2019-03-01 20:32:56 +0100

    Integration du UI SDK

---
+++ sample/src/main/java/co/opensi/kkiapay_sdk/TheApplication.java
@@ -0,0 +1,22 @@
+package co.opensi.kkiapay_sdk;
+
+import android.app.Application;
+
+import co.opensi.kkiapay.uikit.Kkiapay;
+import co.opensi.kkiapay.uikit.SdkConfig;
+
+/**
+ * @author Armel FAGBEDJI ( armel.fagbedji@opensi.co )
+ * created  at 01/03/2019
+ */
+
+public class TheApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Kkiapay.INSTANCE.init(this,
+                "LprYUAyMpfAjq4z2yTHPiY0b6XktIQ",
+                new SdkConfig(R.raw.armoiries, R.color.colorPrimary));
+    }
+}
