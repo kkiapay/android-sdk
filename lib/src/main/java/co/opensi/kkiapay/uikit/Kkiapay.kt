ed94784de0d1055c6e5c01b63eaabdbecab5888d
Shadai ALI <shadai.ali@mehriso.com>
2019-09-02 11:49:13 +0100

    fix sandbox config

--- lib/src/main/java/co/opensi/kkiapay/uikit/Kkiapay.kt
+++ lib/src/main/java/co/opensi/kkiapay/uikit/Kkiapay.kt
@@ -1,6 +1,7 @@
 package co.opensi.kkiapay.uikit
 
 import android.app.Activity
+import android.app.Application
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
